1d4d1be80f6882958d8f602051935af8844dac2c
Jrkeeling23 <jrkeeling23@gmail.com>
2019-02-06 19:33:33 -0700

    tracking bar in progress

--- Assignment_2/object-tracking.py
+++ Assignment_2/object-tracking.py
@@ -35,18 +35,28 @@ def show_cam():
         status, image = capture.read()  # Reads in the capture
         show_unfiltered(image)
         show_hsv(image)
-        # cv.imshow("HSV", cv.cvtColor(image, cv.COLOR_BGR2HSV))    # shows video filtered by hsv
+        create_trackbar(image)
         k = cv.waitKey(1)
         if k == 27:
             break
+
+# TODO Using Sliders create scalers for the min and max values you want to tracka Scalar will be a numpy array (np.array) that takes 3 values for minH, minS, and minV.......then a second scalar to catch the other three Max values create 3 trackbars, createTrackbar with callback methods to set your six variables
+def create_trackbar(image):
+    cv.namedWindow("Track Bar")
+    max_slider_value = 255
+    min_slider_value = 0
+    cv.createTrackbar('Red Max', 'Track Bar', min_slider_value, max_slider_value, none) #TODO add call back value
+    cv.createTrackbar('Green Max', 'Track Bar', min_slider_value, max_slider_value, none) #TODO add call back value
+    cv.createTrackbar('Blue Max', 'Track Bar', min_slider_value, max_slider_value, none) #TODO add call back value
+    cv.imshow('Track Bar', image)
+
+# TODO Us the OpenCV inRange method to find the values between the scalars from HSV image and the result will go to a grayscale image (make it a binary image, white/black).
+
+# TODO Dilate, erode the grayscale image to get a better representation of the object you are tracking.
+
+# TODO Display the original image and the binary image where everything is black except for the object you are tracking. The tracked object will be white.
 
 
 show_cam()
 cv.destroyAllWindows()
 
-# TODO Display the HSV video
-# TODO Click on HSV video and capture the HSV values at the location clicked, and a few other local values of the item you are going to track.use a MouseCallback to print the HSV values.
-# TODO Using Sliders create scalers for the min and max values you want to tracka Scalar will be a numpy array (np.array) that takes 3 values for minH, minS, and minV.......then a second scalar to catch the other three Max values create 3 trackbars, createTrackbar with callback methods to set your six variables
-# TODO Us the OpenCV inRange method to find the values between the scalars from HSV image and the result will go to a grayscale image (make it a binary image, white/black).
-# TODO Dilate, erode the grayscale image to get a better representation of the object you are tracking.
-# TODO Display the original image and the binary image where everything is black except for the object you are tracking. The tracked object will be white.
