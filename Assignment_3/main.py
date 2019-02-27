7b2ce12662113f4fe89bce752cdadd57b7ddb961
Alexander Harry <alex.harry1@gmail.com>
2019-02-27 10:09:15 -0700

    Removed the blog detection function.  Getting the same results with simpler code

--- Assignment_3/main.py
+++ Assignment_3/main.py
@@ -10,29 +10,32 @@ def get_image_range(image, lower, upper):
     circles = cv.HoughCircles(new_image, cv.HOUGH_GRADIENT, 1, new_image.shape[0]/8, param1 = 100, param2=20, minRadius=1, maxRadius=30)
     if circles is not None:
         circles = np.uint16(np.around(circles))
+        count = 0
         for circle in circles[0, :]:
             center = (circle[0], circle[1])
+            count += 1
             cv.circle(new_image, center, 1,  (0,100,100),3)
             radius = circle[2]
             cv.circle(new_image, center, radius, (255,0,255), 3)
     cv.imshow("new image", new_image)
-    return new_image
+
+    return new_image, count
 
 
-def get_keypoints(image):
-    kernel = np.ones((10, 10), np.uint8)
-    image = cv.erode(image, kernel, 1)
-    # image = cv.dilate(image, kernel, 1)
-    params = cv.SimpleBlobDetector_Params()
-    params.filterByArea = True
-    params.minArea = 100
-    params.filterByCircularity = True
-    blob_detection = cv.SimpleBlobDetector_create(params)  # Creates the blob detector
-    return blob_detection.detect(
-        cv.bitwise_not(image))  # Blob detection doesn't work with a black background.  bitwise_not fixes this.
+# def get_keypoints(image):
+#     kernel = np.ones((10, 10), np.uint8)
+#     image = cv.erode(image, kernel, 1)
+#     # image = cv.dilate(image, kernel, 1)
+#     params = cv.SimpleBlobDetector_Params()
+#     params.filterByArea = True
+#     params.minArea = 100
+#     params.filterByCircularity = True
+#     blob_detection = cv.SimpleBlobDetector_create(params)  # Creates the blob detector
+#     return blob_detection.detect(
+#         cv.bitwise_not(image))  # Blob detection doesn't work with a black background.  bitwise_not fixes this.
 
 
-original_image = cv.imread("imagesWOvideo/three.jpg")
+original_image = cv.imread("imagesWOvideo/two.jpg")
 # initial code helped from source: https://thecodacus.com/opencv-object-tracking-colour-detection-python/
 image = cv.GaussianBlur(original_image, (7, 7), 0)  # Blurs the image
 image = cv.cvtColor(image, cv.COLOR_BGR2HSV)  # Converts the image to HSV
@@ -51,27 +54,36 @@ brown_upper = np.array([20, 255, 255])
 red_lower = np.array([0, 0, 0])
 red_upper = np.array([118, 255, 255])
 
-green_image = get_image_range(image, green_lower, green_upper)
-blue_image = get_image_range(image, blue_lower, blue_upper)
-yellow_image = get_image_range(image, yellow_lower, yellow_upper)
-orange_image = get_image_range(image, orange_lower, orange_upper)
-brown_image = get_image_range(image, brown_lower, brown_upper)
-red_image = get_image_range(image, red_lower, red_upper)
+green_image, num_green = get_image_range(image, green_lower, green_upper)
+blue_image, num_blue = get_image_range(image, blue_lower, blue_upper)
+yellow_image, num_yellow = get_image_range(image, yellow_lower, yellow_upper)
+orange_image, num_orange = get_image_range(image, orange_lower, orange_upper)
+brown_image, num_brown = get_image_range(image, brown_lower, brown_upper)
+red_image, num_red = get_image_range(image, red_lower, red_upper)
 
-num_blue = get_keypoints(blue_image)
-num_green = get_keypoints(green_image)
-num_yellow = get_keypoints(yellow_image)
-num_orange = get_keypoints(orange_image)
-num_brown = get_keypoints(brown_image)
-num_red = get_keypoints(red_image)
+# num_blue = get_keypoints(blue_image)
+# num_green = get_keypoints(green_image)
+# num_yellow = get_keypoints(yellow_image)
+# num_orange = get_keypoints(orange_image)
+# num_brown = get_keypoints(brown_image)
+# num_red = get_keypoints(red_image)
 
-print(len(num_blue))
-cv.putText(original_image, "Blue: " + str(len(num_blue)), (10, 50), cv.FONT_HERSHEY_PLAIN, 2, [255, 255, 255])
-cv.putText(original_image, "Green: " + str(len(num_green)), (200, 50), cv.FONT_HERSHEY_PLAIN, 2, [255, 255, 255])
-cv.putText(original_image, "Yellow: " + str(len(num_yellow)), (10, 100), cv.FONT_HERSHEY_PLAIN, 2, [255, 255, 255])
-cv.putText(original_image, "Brown: " + str(len(num_brown)), (200, 100), cv.FONT_HERSHEY_PLAIN, 2, [255, 255, 255])
-cv.putText(original_image, "Orange: " + str(len(num_orange)), (10, 150), cv.FONT_HERSHEY_PLAIN, 2, [255, 255, 255])
-cv.putText(original_image, "Red: " + str(len(num_red)), (200, 150), cv.FONT_HERSHEY_PLAIN, 2, [255, 255, 255])
+# print(len(num_blue))
+# cv.putText(original_image, "Blue: " + str(len(num_blue)), (10, 50), cv.FONT_HERSHEY_PLAIN, 2, [255, 255, 255])
+# cv.putText(original_image, "Green: " + str(len(num_green)), (200, 50), cv.FONT_HERSHEY_PLAIN, 2, [255, 255, 255])
+# cv.putText(original_image, "Yellow: " + str(len(num_yellow)), (10, 100), cv.FONT_HERSHEY_PLAIN, 2, [255, 255, 255])
+# cv.putText(original_image, "Brown: " + str(len(num_brown)), (200, 100), cv.FONT_HERSHEY_PLAIN, 2, [255, 255, 255])cv.putText(original_image, "Blue: " + str(len(num_blue)), (10, 50), cv.FONT_HERSHEY_PLAIN, 2, [255, 255, 255])
+# cv.putText(original_image, "Orange: " + str(len(num_orange)), (10, 150), cv.FONT_HERSHEY_PLAIN, 2, [255, 255, 255])
+# cv.putText(original_image, "Red: " + str(len(num_red)), (200, 150), cv.FONT_HERSHEY_PLAIN, 2, [255, 255, 255])
+
+cv.putText(original_image, "Blue: " + str(num_blue), (10, 50), cv.FONT_HERSHEY_PLAIN, 2, [255, 255, 255])
+cv.putText(original_image, "Green: " + str(num_green), (200, 50), cv.FONT_HERSHEY_PLAIN, 2, [255, 255, 255])
+cv.putText(original_image, "Yellow: " + str(num_yellow), (10, 100), cv.FONT_HERSHEY_PLAIN, 2, [255, 255, 255])
+cv.putText(original_image, "Brown: " + str(num_brown), (200, 100), cv.FONT_HERSHEY_PLAIN, 2, [255, 255, 255])
+cv.putText(original_image, "Orange: " + str(num_orange), (10, 150), cv.FONT_HERSHEY_PLAIN, 2, [255, 255, 255])
+cv.putText(original_image, "Red: " + str(num_red), (200, 150), cv.FONT_HERSHEY_PLAIN, 2, [255, 255, 255])
+
+
 
 cv.imshow("Candy Image original", original_image)
 
