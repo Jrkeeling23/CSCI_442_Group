9ef45b691b447303c9eb0abd9978d65a00b96b6b
Alex <alex.harry1@gmail.com>
2019-04-14 00:43:16 -0600

    added rectangle to show highest point

--- final/main.py
+++ final/main.py
@@ -21,7 +21,7 @@ class Driver:
             image = self.manipulation.edge_detection(img.copy())
             image = self.manipulation.fill_image(image)
             image = self.manipulation.smooth(image)
-            print
+
             image, x_coordinate, y_coordinate = self.manipulation.getHighestCoordinate(image, int(self.width / 2), self.height)
             # self.move.decide_move(x_coordinate, y_coordinate)
             overlayed = cv.addWeighted(img, .7, image, 0.4, 0)  # Overlays the path on the original image
