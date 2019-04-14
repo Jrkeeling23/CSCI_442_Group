b1fbd8dc17c651c49118c328eeb5e6ec3fae7a17
Alex <alex.harry1@gmail.com>
2019-04-13 22:58:13 -0600

    Added functionality for robot movement

--- final/makeMoves.py
+++ final/makeMoves.py
@@ -5,17 +5,25 @@ class Move:
         self.forward_val = int(height * .8)
         self.turn_left_val = int(width * 0.55)
         self.turn_right_val = int(width * 0.45)
-   #     self.robot = robot_control.MoveRobot()
+        self.robot = robot_control.MoveRobot()
 
     def decide_move(self,x_val, y_val):
         print(self.forward_val)
         print(y_val)
         if x_val is not None:
             if x_val < self.turn_right_val:
-                print("turn right")
+                #print("turn right")
+                self.robot.turn_right()
                 return
             elif x_val > self.turn_left_val:
-                print("turn left")
+                #print("turn left")
+                self.robot.turn_left()
                 return
-        if y_val is not None and y_val < self.forward_val:
-            print("move Forward")
+            elif self.robot.turn != 6000:
+                self.robot.stop()
+        if y_val is not None:
+            #print("move Forward")
+            if y_val < self.forward_val:
+                self.robot.wheels_forward()
+            elif self.robot.motors != 6000:
+                self.robot.stop()
